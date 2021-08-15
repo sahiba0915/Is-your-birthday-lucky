cbbb22613248188c058c3749cd5970096b4b5409
Sahiba Kumari <sahibakumari0915@outlook.com>
2021-08-15 10:08:10 +0530

    style: did the styling of the page.

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check");
-const output      = document.querySelector("#output-box");
+const output = document.querySelector("#output-box");
 
-checkNumber.addEventListener('click',checkIsLucky);
+checkNumber.addEventListener('click', checkIsLucky);
 
-function calculateSum(dob){
+function calculateSum(dob) {
     dob = dob.replaceAll("-", "");
     let sum = 0;
-    for(let i =0; i < dob.length; i++){
+    for (let i = 0; i < dob.length; i++) {
         sum = sum + Number(dob.charAt(i));
     }
     return sum;
 }
 
 
-function checkIsLucky(){
+function checkIsLucky() {
     const dob = dateOfBirth.value;
-    const sum =  calculateSum(dob);
-    if(sum&&compareValues){
+    const sum = calculateSum(dob);
+    if (sum && compareValues) {
         compareValues(sum, luckyNumber.value);
-    }else{
+    } else {
         output.innerText = "Please fill both the boxes"
     }
-    
+
 }
 
-function compareValues(sum, luckyNumber){
-    if(sum%luckyNumber === 0){
-         output.innerText = "Your birthday is lucky🎉"
-    }
-    else{
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        output.innerText = "Your birthday is lucky🎉"
+    } else {
         output.innerText = "Your birthday is not lucky 😔"
     }
 }
